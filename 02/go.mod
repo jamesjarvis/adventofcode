a9d27495b44bf1525bd155385a88411bd6c2cac1
James Jarvis <git@jamesjarvis.io>
2021-12-04 20:25:28 +0000

    day 2 part 1

---
+++ 02/go.mod
@@ -0,0 +1,3 @@
+module github.com/jamesjarvis/adventofcode/02
+
+go 1.17
---
+++ 02/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+//go:embed input.txt
+var input string
+
+type command struct {
+	bearing  string
+	distance int
+}
+
+func main() {
+	if input == "" {
+		panic("input cannot be empty")
+	}
+	inputstrarr := strings.Split(input, "\n")
+	inputarr := make([]command, len(inputstrarr))
+	for i, v := range inputstrarr {
+		commandarr := strings.Split(v, " ")
+		n, err := strconv.Atoi(commandarr[1])
+		if err != nil {
+			panic(err)
+		}
+		c := command{
+			bearing:  commandarr[0],
+			distance: n,
+		}
+		inputarr[i] = c
+	}
+
+	horizontal, depth := getPosFromCommands(inputarr)
+	fmt.Printf("Answer: %d\n", horizontal*depth)
+}
+
+func getPosFromCommands(vals []command) (horizontalPos int, depth int) {
+	for _, c := range vals {
+		switch c.bearing {
+		case "forward":
+			horizontalPos += c.distance
+		case "down":
+			depth += c.distance
+		case "up":
+			depth -= c.distance
+		default:
+			fmt.Println("what da fuk", c.bearing)
+		}
+	}
+	return horizontalPos, depth
+}
