9eee4fa112ddab6a4415942ce3e3caf40e7856fb
James Jarvis <git@jamesjarvis.io>
2021-12-06 23:13:29 +0000

    day 6 part 1 intro

---
+++ 06/go.mod
@@ -0,0 +1,3 @@
+module github.com/jamesjarvis/adventofcode/06
+
+go 1.17
---
+++ 06/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+//go:embed input.txt
+var input string
+
+const simulateNDays = 80
+const eachFishReplicatesEveryNDays = 7
+
+func main() {
+	if input == "" {
+		panic("input cannot be empty")
+	}
+	inputstrarr := strings.Split(input, ",")
+	world := &World{
+		feesh: []*Lanternfish{},
+	}
+	for _, val := range inputstrarr {
+		n, err := strconv.Atoi(val)
+		if err != nil {
+			panic(err)
+		}
+
+		world.feesh = append(world.feesh, &Lanternfish{internaltimer: n})
+	}
+
+	for i := 0; i < simulateNDays; i++ {
+		world.Tick()
+	}
+
+	numberOfFish := world.Size()
+	fmt.Printf("Answer: %d\n", numberOfFish)
+}
+
+type World struct {
+	feesh []*Lanternfish
+
+	day int
+}
+
+func (w *World) Size() int { return len(w.feesh) }
+
+func (w *World) Tick() {
+	newFeeshToAdd := []*Lanternfish{}
+	for _, lf := range w.feesh {
+		newFeesh := lf.Tick()
+		if newFeesh != nil {
+			newFeeshToAdd = append(newFeeshToAdd, newFeesh)
+		}
+	}
+	w.feesh = append(w.feesh, newFeeshToAdd...)
+	w.day++
+}
+
+type Lanternfish struct {
+	internaltimer int
+}
+
+func (lf *Lanternfish) Tick() *Lanternfish {
+	if lf.internaltimer == 0 {
+		lf.internaltimer = eachFishReplicatesEveryNDays - 1
+		return &Lanternfish{eachFishReplicatesEveryNDays + 1}
+	}
+	lf.internaltimer = lf.internaltimer - 1
+	return nil
+}
